3d190a4708dd861198154b5ef463b5c3f3f7f8eb
dinh.thuy.duong <dinh.thuy.duong@fujitsu.com>
2023-06-14 16:19:03 +0900

    Add Apple Auth

--- Assets/script/UserManagement.cs
+++ Assets/script/UserManagement.cs
@@ -8,6 +8,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 // using UnityEngine.UIElements;
 
+
 public class UserManagement : MonoBehaviour
 {
     public InputField user;
@@ -18,6 +19,8 @@ public class UserManagement : MonoBehaviour
     public Button btnAddMemberToRole;
     public Button btnRemoveFromRole;
     public Button btnAddChildRole;
+    public Button btnAppleAuthRegister;
+    private NCMBAppleAuthenManager appleAuthenManager;
     private bool isLogedIn = false;
     // Start is called before the first frame update
     void Start()
@@ -38,14 +41,48 @@ public class UserManagement : MonoBehaviour
             isLogedIn = false;
         }
 
+        this.appleAuthenManager = new NCMBAppleAuthenManager();
         SetActive();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if (this.appleAuthenManager != null)
+      {
+          this.appleAuthenManager.Update();
+      }
 
+    }
+
+    public void SignInWithAppleIdPress()
+    {
+        // iOSのネイティブアプリ側のSignInWithAppleID処理を実施
+        this.appleAuthenManager.NCMBiOSNativeLoginWithAppleId(
+            credential =>
+            {
+                // Handler the result and LogInWithAuthDataAsync in Mbaas.
+                NCMBAppleParameters appleParameters = new NCMBAppleParameters(credential.UserId, credential.AuthorizationCode, "fjct.nifcloud.mbaas.testios13");
+                NCMBUser user = new NCMBUser();
+                user.AuthData = appleParameters.param;
+                user.LogInWithAuthDataAsync((NCMBException e) => {
+                    if (e != null)
+                    {
+                        // 認証失敗時の処理
+                        Debug.Log ("エラー: " + e.ErrorMessage);
+                    }
+                    else
+                    {
+                        // 認証成功時の処理
+                        Debug.Log("会員認証完了しました。");
+                    }
+
+                });
+            },
+            error =>
+            {
+                Debug.Log("エラー: " + error.Code);
+            });
     }
 
     public void SetActive()
@@ -80,7 +117,7 @@ public class UserManagement : MonoBehaviour
             if (IsEmail(user.text))
             {
                 NCMBUser.LogInWithMailAddressAsync(user.text, password.text, (NCMBException e) => {
-                    if (e != null) 
+                    if (e != null)
                     {
                         if (e.ErrorCode.CompareTo("E400003") == 0)
                         {
@@ -90,9 +127,9 @@ public class UserManagement : MonoBehaviour
                             Debug.Log("ログインに失敗: " + e.ErrorMessage);
 
                         }
-                        
 
-                    } 
+
+                    }
                     else
                     {
                         isLogedIn = true;
@@ -125,13 +162,13 @@ public class UserManagement : MonoBehaviour
                     SetActive();
                 });
             }
-            
+
         }
     }
 
     public void SignUp()
     {
-        //NCMBUserのインスタンス作成 
+        //NCMBUserのインスタンス作成
         NCMBUser us = new NCMBUser();
         //ユーザ名とパスワードの設定
         if (IsEmail(user.text))
@@ -185,7 +222,26 @@ public class UserManagement : MonoBehaviour
                 }
             });
         }
-        
+
+    }
+
+    public void AppleAuthRegister()
+    {
+      UnityEngine.Debug.Log("Apple Authentication start");
+        // if (IsEmail(user.text))
+        // {
+        //     NCMBUser.RequestAuthenticationMailAsync(user.text, (NCMBException e) => {
+        //         if (e != null)
+        //         {
+        //             UnityEngine.Debug.Log("新規登録に失敗: " + e.ErrorMessage);
+        //         }
+        //         else
+        //         {
+        //             UnityEngine.Debug.Log("新規登録に成功");
+        //         }
+        //     });
+        // }
+
     }
 
     public void AddToRole()
@@ -259,7 +315,7 @@ public class UserManagement : MonoBehaviour
                                     bronzeRole.SaveAsync();
                                 }
                             }
-                            
+
                         }
                     });
                 }
